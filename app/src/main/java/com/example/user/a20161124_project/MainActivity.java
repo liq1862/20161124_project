52a9654bcd971dc16772f6c78edee5910159bba3
lili <liq1862@gmail.com>
2016-11-28 16:27:54 +0800

    加入圖表

--- app/src/main/java/com/example/user/a20161124_project/MainActivity.java
+++ app/src/main/java/com/example/user/a20161124_project/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.user.a20161124_project;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -24,12 +27,12 @@ public class MainActivity extends AppCompatActivity {
     TextView tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        WebView wv;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tv1 = (TextView) findViewById(R.id.textView);
         tv2 = (TextView) findViewById(R.id.textView2);
-
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         StringRequest request = new StringRequest("https://api.thingspeak.com/channels/176124/fields/1.json?results=1&api_key=9AK9G8B8BN9GKIK8&timezone=Asia/Taipei",
                 new Response.Listener<String>() {
@@ -61,7 +64,16 @@ public class MainActivity extends AppCompatActivity {
         });
         queue.add(request);
         queue.start();
+        //暫時關掉權限(僅可測試用),
+        StrictMode.VmPolicy policy = new StrictMode.VmPolicy.Builder()
+                .detectFileUriExposure()
+                .build();
+        StrictMode.setVmPolicy(policy);
 
+        wv = (WebView) findViewById(R.id.webView);
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("https://thingspeak.com/channels/176124/charts/1?api_key=9AK9G8B8BN9GKIK8&bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line&width=1& height=1");
     }
 }
 
